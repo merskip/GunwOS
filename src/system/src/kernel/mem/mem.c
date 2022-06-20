be25a8b47188c399981bac4b71ccddbd9529119e
Artur Danielewski <artur.danielewski@gmail.com>
2022-06-20 18:34:22 +0200

    commented mem.c and mem.h

---
+++ src/system/src/kernel/mem/mem.c
@@ -0,0 +1,14 @@
+//
+//  mem.c
+//  GunwOS
+//
+//  Created by Artur Danielewski on 20.01.2018.
+//
+
+#include "mem.h"
+
+// static unsigned int MEMMGR_BASE_ADDR    = 0x200000;
+
+// void *k_mem_alloc(size_t size) {
+    // return (void *)(MEMMGR_BASE_ADDR += size);
+// }
---
+++ src/system/src/kernel/mem/mem.h
@@ -0,0 +1,18 @@
+//
+//  mem.h
+//  GunwOS
+//
+//  Created by Artur Danielewski on 20.01.2018.
+//
+
+#include "../../../../lib/stdgunw/types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// void *k_mem_alloc(size_t size);
+
+#ifdef __cplusplus
+};
+#endif
